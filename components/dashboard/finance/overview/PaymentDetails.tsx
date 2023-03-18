152bed76dd35e0a7e17be0af2a5f6f172f940c23
Jamesalehandro <jamesalehandro@gmail.com>
2023-03-18 20:38:23 +0000

    Updated

--- components/dashboard/finance/overview/PaymentDetails.tsx
+++ components/dashboard/finance/overview/PaymentDetails.tsx
@@ -1,12 +1,53 @@
-import { GridItem } from '@chakra-ui/react';
+import { useSharedColor } from '@/customHooks';
+import { getDay } from '@/utils';
+import { Center, Flex, GridItem, Text } from '@chakra-ui/react';
+import { BsFillCreditCardFill } from 'react-icons/bs';
+import { WiMoonAltWaxingCrescent6 } from 'react-icons/wi';
+import { TandaVDivider } from '../../shared';
+import { FinanceCard } from '../shared/FinanceCard';
 
 export const PaymentDetails = () => {
+  const color = useSharedColor();
+
   return (
     <GridItem
       minH='176px'
       layerStyle='gridItem'
       colSpan={{ base: 1, md: 2 }}
       gap='7'
-    ></GridItem>
+    >
+      <Flex align='center' gap='10'>
+        <FinanceCard
+          title='Next Payment'
+          desc='$22,644,94'
+          info='Due in 28 day(s)'
+          icon={BsFillCreditCardFill}
+          extra={
+            <Text textStyle='title' fontSize='0.9rem' color={color}>
+              {getDay(true)}
+            </Text>
+          }
+        />
+
+        <TandaVDivider />
+
+        <FinanceCard
+          title='Outstanding'
+          desc='$22,644,94'
+          info='Due in 28 day(s)'
+          icon={WiMoonAltWaxingCrescent6}
+          extra={
+            <Center
+              border='10px solid #F0EAFD'
+              w='72px'
+              h='72px'
+              borderRadius='full'
+            >
+              0%
+            </Center>
+          }
+        />
+      </Flex>
+    </GridItem>
   );
 };
