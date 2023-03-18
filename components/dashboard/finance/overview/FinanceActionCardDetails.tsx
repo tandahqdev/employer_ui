099ae222290698f172faf8321a397f20ec05c020
Jamesalehandro <jamesalehandro@gmail.com>
2023-03-18 23:13:48 +0000

    Updataed

---
+++ components/dashboard/finance/overview/FinanceActionCardDetails.tsx
@@ -0,0 +1,73 @@
+import { ColumnFlex } from '@/components/shared';
+import { DEFAULT_STYLES } from '@/styles';
+import { BackgroundProps, Box, Flex, Icon, Text } from '@chakra-ui/react';
+import { RiErrorWarningLine } from 'react-icons/ri';
+import { WiMoonAltWaxingCrescent6 } from 'react-icons/wi';
+
+interface Props {
+  title: string;
+  subTitle: string;
+  priceDesc?: string;
+  bg?: BackgroundProps['bg'];
+  percent?: string;
+}
+
+export const FinanceActionCardDetails = ({
+  subTitle,
+  title,
+  bg,
+  priceDesc,
+  percent,
+}: Props) => {
+  return (
+    <ColumnFlex width={{ base: '90%', lg: '85%' }} py='6' gap='4'>
+      <Flex align='center' gap='3'>
+        <Icon
+          as={WiMoonAltWaxingCrescent6}
+          fontSize='1.3rem'
+          color={DEFAULT_STYLES.darkGray}
+        />
+
+        <Flex align='center' gap='1.5'>
+          <Text
+            fontSize='1rem'
+            fontWeight={DEFAULT_STYLES.bold}
+            color={DEFAULT_STYLES.primaryColor}
+            opacity={0.4}
+          >
+            {title}
+          </Text>
+          <Icon as={RiErrorWarningLine} color='#A3A7B7' />
+        </Flex>
+      </Flex>
+
+      <Flex align='center' gap='3' justify='space-between'>
+        <Text textStyle='title' fontSize='0.8rem'>
+          {subTitle}
+        </Text>
+
+        {priceDesc && (
+          <Text
+            textStyle='desc'
+            color={DEFAULT_STYLES.primaryColor}
+            opacity={0.4}
+          >
+            From {priceDesc}
+          </Text>
+        )}
+      </Flex>
+
+      {bg && (
+        <Box w={DEFAULT_STYLES.fullWidth} h='13px' bg='#DDE0F1' rounded={39}>
+          <Box
+            w={`${percent ?? '0'}%`}
+            transition='all 0.5s ease-in-out'
+            h='full'
+            bg={bg}
+            rounded={39}
+          />
+        </Box>
+      )}
+    </ColumnFlex>
+  );
+};
--- components/dashboard/finance/overview/pages/SecondMainPage.tsx
+++ components/dashboard/finance/overview/pages/SecondMainPage.tsx
@@ -1,13 +1,18 @@
-import { ActionCard } from '@/components/dashboard/shared';
+import { ActionCard, TandaHDivider } from '@/components';
 import { growIcon, extendIcon } from '@/store';
 import { DEFAULT_STYLES } from '@/styles';
 import { DashRoutes } from '@/utils';
 import { AddIcon } from '@chakra-ui/icons';
+import { FinanceActionCardDetails } from '../FinanceActionCardDetails';
 import { PaymentDetails } from '../PaymentDetails';
 import { SharedFinanceOverview } from '../SharedFinanceOverview';
 import { Transaction } from '../Transaction';
 
 export const SecondMainPage = () => {
+  // Calculations to use for the bar percent
+  const percent = (5000 / 100000) * 100;
+  const percent2 = (25000 / 100000) * 100;
+
   return (
     <SharedFinanceOverview>
       <PaymentDetails />
@@ -22,6 +27,21 @@ export const SecondMainPage = () => {
         leftIcon={<AddIcon />}
         btnBg={DEFAULT_STYLES.lightPurple}
         info='2 active draws'
+        details={
+          <>
+            <FinanceActionCardDetails
+              title='Outstanding'
+              subTitle='$5,000'
+              priceDesc='$100,000'
+              bg='linear-gradient(90deg, #6631FF 43.06%, rgba(244, 214, 94, 0.5) 94.79%)'
+              percent={`${percent}`}
+            />
+
+            <TandaHDivider />
+
+            <FinanceActionCardDetails title='Rate' subTitle='12.00%' />
+          </>
+        }
       />
 
       <ActionCard
@@ -34,6 +54,21 @@ export const SecondMainPage = () => {
         leftIcon={<AddIcon />}
         btnBg={DEFAULT_STYLES.pink}
         info='2 active draws'
+        details={
+          <>
+            <FinanceActionCardDetails
+              title='Outstanding'
+              subTitle='$25,000'
+              priceDesc='$100,000'
+              bg='linear-gradient(270deg, #EF567E 50%, rgba(239, 86, 126, 0.64) 63.42%);'
+              percent={`${percent2}`}
+            />
+
+            <TandaHDivider />
+
+            <FinanceActionCardDetails title='Rate' subTitle='12.00%' />
+          </>
+        }
       />
 
       <Transaction buttons={['Upcoming Debits', 'Complete Transactions']} />
