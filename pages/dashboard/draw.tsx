42610978064a88799a18ec65f9e7eb309b892560
Jamesalehandro <jamesalehandro@gmail.com>
2023-03-19 22:14:08 +0000

    Fixes

--- pages/dashboard/draw.tsx
+++ pages/dashboard/draw.tsx
@@ -13,7 +13,9 @@ import {
 } from '@chakra-ui/react';
 
 const DashboardDraw = () => {
-  const topbar = <Header>New draw set up</Header>;
+  const topbar = (
+    <Header fontSize={{ base: '1.1rem', md: '1.5rem' }}>New draw set up</Header>
+  );
 
   const renderTableHeaders = [
     'Month',
@@ -40,7 +42,7 @@ const DashboardDraw = () => {
         gap={{ base: '33px', lg: '10px', xl: '33px' }}
         templateColumns={{
           base: '1fr',
-          md: 'repeat(2,1fr)',
+          md: 'repeat(3,1fr)',
           lg: 'repeat(2,600px 330px)',
           xl: 'repeat(2,627px 340px)',
           '2xl': 'repeat(2,727px 424px)',
@@ -50,11 +52,12 @@ const DashboardDraw = () => {
         w={DEFAULT_STYLES.fullWidth}
       >
         <GridItem
-          colSpan={{ base: 1, lg: 'auto' }}
+          colSpan={{ base: 1, md: 3, lg: 'auto' }}
           minH='417px'
           layerStyle='gridItem'
           justifyContent='flex-start'
           gap='50px'
+          minW={DEFAULT_STYLES.fullWidth}
         >
           <Text
             textStyle='title'
@@ -74,9 +77,11 @@ const DashboardDraw = () => {
         </GridItem>
 
         <GridItem
-          colSpan={{ base: 1, lg: 'auto' }}
+          colSpan={{ base: 1, md: 'auto', lg: 'auto' }}
           minH='598px'
+          width={{ base: 'auto', md: '374px', lg: 'auto' }}
           layerStyle='gridItem'
+          gridRowStart={{ base: 1, lg: 'auto' }}
         ></GridItem>
       </Grid>
     </DashBoardLayout>
