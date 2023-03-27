82d3915409707245dc00d1294ed8905b762ab5f7
Jamesalehandro <jamesalehandro@gmail.com>
2023-03-27 22:16:02 +0100

    test

--- components/shared/ColumnFlex.tsx
+++ components/shared/ColumnFlex.tsx
@@ -3,6 +3,9 @@ import { BoxProps, Flex, FlexProps } from '@chakra-ui/react';
 interface Props extends FlexProps, BoxProps {}
 
 export const ColumnFlex = ({ children, ...rest }: Props) => {
+  const t = [];
+
+  
   return (
     <Flex direction={rest.direction ?? 'column'} {...rest}>
       {children}
