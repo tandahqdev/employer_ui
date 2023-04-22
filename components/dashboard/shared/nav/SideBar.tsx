a596fa7943fbba7c690ccb1a1703cd37afc8672e
Jamesalehandro <jamesalehandro@gmail.com>
2023-04-22 20:56:52 +0100

    new sidebar

--- components/dashboard/shared/nav/SideBar.tsx
+++ components/dashboard/shared/nav/SideBar.tsx
@@ -13,77 +13,90 @@ import { DashRoutes } from '@/utils';
 import { useRouter } from 'next/router';
 import { CloseIcon } from '@chakra-ui/icons';
 import { useCustomMediaQuery } from '@/context';
+import { Flex } from '@chakra-ui/react';
 
 // TODO: Remove nextjs data collection
-// TODO: Change the stack in the admin side bar to be flex 1.
 
 export const SideBar = () => {
   const { push } = useRouter();
   const { mediaQuery, upDateMediaQuery } = useCustomMediaQuery();
 
   return (
-    <ColumnFlex
-      w='200px'
-      h={DEFAULT_STYLES.fullHeight}
+    <Flex
+      w={{ base: 'full', xl: 'max-content' }}
+      bg='linear-gradient(180deg, rgba(23, 31, 52, 0.25) 0%, rgba(23, 31, 52, 0.25) 100%)'
+      backdropFilter='blur(5px)'
       position='fixed'
+      h={DEFAULT_STYLES.fullWidth}
       zIndex={10}
-      bg={DEFAULT_STYLES.white}
-      shadow='0px 1px 12px rgba(195, 203, 222, 0.58806)'
-      px='0.5'
-      pt='40px'
-      transform={{
-        base: `translate( ${mediaQuery ? '0px' : '-200px'})`,
-        xl: `translate(0px)`,
-      }}
       transition={DEFAULT_STYLES.transition}
+      transform={{
+        base: `translate( ${mediaQuery ? '0%' : '-100%'})`,
+        xl: 'translate(0px)',
+      }}
+      onClick={() => {
+        upDateMediaQuery(false);
+      }}
     >
-      <IconBtn
-        aria-label='Close button'
-        icon={<CloseIcon />}
-        bgColor='transparent'
-        display={{ base: 'flex', xl: 'none' }}
-        position='absolute'
-        right='5'
-        top='4'
-        onClick={() => {
-          upDateMediaQuery(false);
-        }}
-      />
-      <Logo />
-
-      <ColumnFlex overflow='auto' gap='1' flex='1' pb='20px' mt='30px'>
-        <ColumnFlex gap='1'>
-          <NavItem
-            icon={AiTwotoneHome}
-            title='Home'
-            path={DashRoutes.dashboard}
-          />
-
-          <TandaHDivider />
-
-          <NavCard header='Financing' list={financeItems} />
-
-          <TandaHDivider />
-
-          <NavCard header='Banking' list={bankingItems} />
-        </ColumnFlex>
-
-        <HelpCard />
-
-        <NavItem
-          icon={FiSettings}
-          title='Settings'
-          path={DashRoutes.settings}
-        />
-
-        <NavItem
-          icon={BiLogOut}
-          title='Logout'
+      <ColumnFlex
+        w='200px'
+        h={DEFAULT_STYLES.fullWidth}
+        position='relative'
+        bg={DEFAULT_STYLES.white}
+        shadow='0px 1px 12px rgba(195, 203, 222, 0.58806)'
+        px='0.5'
+        pt='40px'
+        transition={DEFAULT_STYLES.transition}
+      >
+        <IconBtn
+          aria-label='Close button'
+          icon={<CloseIcon />}
+          bgColor='transparent'
+          display={{ base: 'flex', xl: 'none' }}
+          position='absolute'
+          right='5'
+          top='4'
           onClick={() => {
-            push(DashRoutes.home);
+            upDateMediaQuery(false);
           }}
         />
+
+        <Logo />
+
+        <ColumnFlex overflowY='auto' gap='1' flex='1' pb='20px' mt='30px'>
+          <ColumnFlex gap='1' flex='1'>
+            <NavItem
+              icon={AiTwotoneHome}
+              title='Home'
+              path={DashRoutes.dashboard}
+            />
+
+            <TandaHDivider />
+
+            <NavCard header='Financing' list={financeItems} />
+
+            <TandaHDivider />
+
+            <NavCard header='Banking' list={bankingItems} />
+          </ColumnFlex>
+
+          <HelpCard />
+
+          <NavItem
+            icon={FiSettings}
+            title='Settings'
+            path={DashRoutes.settings}
+          />
+
+          <NavItem
+            icon={BiLogOut}
+            title='Logout'
+            onClick={() => {
+              push(DashRoutes.home);
+            }}
+          />
+        </ColumnFlex>
       </ColumnFlex>
-    </ColumnFlex>
+    </Flex>
   );
 };
